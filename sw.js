e8613848e8d45aa470c10495e413db4d478085f3
gustavo castanares <115637674+shutten232@users.noreply.github.com>
2026-05-19 12:13:52 -0300

    veamos

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'crpc-pwa-v3';
+const APP_SHELL = ['./'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(()=>{}));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./')))
+  );
+});
